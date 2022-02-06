80e00db2e99cd42042a8dbbe21e38d04ca8d143d
Fulia Bai <fulia@Fulias-MacBook-Pro.local>
2022-02-05 22:16:06 -0800

    added onclick events for buttons in clicky activity

--- app/src/main/java/com/example/numad22sp_xuebai/ClickyActivity.java
+++ app/src/main/java/com/example/numad22sp_xuebai/ClickyActivity.java
@@ -3,12 +3,61 @@ package com.example.numad22sp_xuebai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ClickyActivity extends AppCompatActivity {
+// implements interface View.onClickListener first then use switch to implement onClick method
+public class ClickyActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView pressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky);
+
+        pressed = findViewById(R.id.txtPressed);
+        Button btnA = findViewById(R.id.btnA);
+        Button btnB = findViewById(R.id.btnB);
+        Button btnC = findViewById(R.id.btnC);
+        Button btnD = findViewById(R.id.btnD);
+        Button btnE = findViewById(R.id.btnE);
+        Button btnF = findViewById(R.id.btnF);
+
+        btnA.setOnClickListener(this);
+        btnB.setOnClickListener(this);
+        btnC.setOnClickListener(this);
+        btnD.setOnClickListener(this);
+        btnE.setOnClickListener(this);
+        btnF.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnA:
+                pressed.setText("Pressed: A");
+                break;
+            case R.id.btnB:
+                pressed.setText("Pressed: B");
+                break;
+            case R.id.btnC:
+                pressed.setText("Pressed: C");
+                break;
+            case R.id.btnD:
+                pressed.setText("Pressed: D");
+                break;
+            case R.id.btnE:
+                pressed.setText("Pressed: E");
+                break;
+            case R.id.btnF:
+                pressed.setText("Pressed: F");
+                break;
+            default:
+                pressed.setText("Pressed: -");
+        }
+
     }
 }
--- app/src/main/java/com/example/numad22sp_xuebai/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_xuebai/MainActivity.java
@@ -2,29 +2,53 @@ package com.example.numad22sp_xuebai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // add a btnClicky
+        // button About Me
         Button btnAboutMe = findViewById(R.id.btnAbout);
+        // button Clicky Clicky
+        Button btnClicky = findViewById(R.id.btnClicky);
+
         // action onClick to display some text
-        btnAboutMe.setOnClickListener(new View.OnClickListener() {
-            // abstract class
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hi! My name is Xue Bai and you can contact me" +
-                        "at bai.xue1@northeastern.edu. Nice to meet you!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        btnAboutMe.setOnClickListener(this);
+        btnClicky.setOnClickListener(this);
+
+//        replaced by the switch
+//        btnAboutMe.setOnClickListener(new View.OnClickListener() {
+//            // abstract class
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Hi! My name is Xue Bai and you can contact me" +
+//                        "at bai.xue1@northeastern.edu. Nice to meet you!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnAbout:
+                Toast.makeText(MainActivity.this, "Hi! My name is Xue Bai and you can contact me" +
+                        "at bai.xue1@northeastern.edu. Nice to meet you!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnClicky:
+                // start a new activity after clicking a button
+                // Intent: an abstract description of an operation to be performed.
+                Intent intent = new Intent(this, ClickyActivity.class);
+                startActivity(intent);
+
+        }
+    }
 }
