b649685f0a626eb4a6a4b4636e67bb72e9e005a6
Fulia Bai <fulia@Fulias-MacBook-Pro.local>
2022-02-05 19:47:39 -0800

    added activity for clicky button

---
+++ app/src/main/java/com/example/numad22sp_xuebai/ClickyActivity.java
@@ -0,0 +1,14 @@
+package com.example.numad22sp_xuebai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky);
+    }
+}
