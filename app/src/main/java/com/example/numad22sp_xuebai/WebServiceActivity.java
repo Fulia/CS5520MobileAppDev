4d5cad2899b457a03e6ecbcf8991b499c5dd5852
Fulia Bai <fulia@Fulias-MacBook-Pro.local>
2022-03-06 18:17:20 -0800

    added WebService activity

--- app/src/main/java/com/example/numad22sp_xuebai/WebServiceActivity.java
+++ app/src/main/java/com/example/numad22sp_xuebai/WebServiceActivity.java
@@ -1,4 +1,14 @@
 package com.example.numad22sp_xuebai;
 
-public class WebServiceActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_service);
+    }
 }
